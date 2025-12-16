a18fd4c3c858aae8b84b3278944fd1b9a9ab2786
qalmurri2 <blog.qalmurri@gmail.com>
2025-12-16 08:01:04 +0700

    modified:   backend/scripts/repositories/size.py

--- backend/scripts/repositories/size.py
+++ backend/scripts/repositories/size.py
@@ -4,18 +4,15 @@ from django.shortcuts import get_object_or_404
 class SizeQueryRepository:
     @staticmethod
     def list_all():
-        """Jumuk kabeh data e seng ndek tabel Size"""
         return Size.objects.all()
     
     @staticmethod
     def get_by_id(id: int) -> Size:
-        """jumuk data teko id"""
         return get_object_or_404(Size, id=id)
     
 class SizeCommandRepository:
     @staticmethod
     def create(size: str) -> Size:
-        """gae data, payload e mek size tok, id otomatis kegawe"""
         return Size.objects.create(
             size=size
         )
