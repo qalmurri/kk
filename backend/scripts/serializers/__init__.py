0dd12f51986b23e0d76b7fe48e18fc608b94aa55
qalmurri <blog.qalmurri@gmail.com>
2025-12-25 18:48:15 +0700

    modified:   backend/scripts/serializers/__init__.py

--- backend/scripts/serializers/__init__.py
+++ backend/scripts/serializers/__init__.py
@@ -1,13 +1,72 @@
 from .user import User
 from .activity import ActivitySerializer
-from .base import BaseCompactSerializer, PolicyBasedSerializer, BaseReadSerializer
-from .common import Institute, Size
-from .cover import Cover
-from .description import SectionDescription as DescriptionPart, Description, TextDescription as Text
-from .flag import Part, Flag
-from .isbn import Type, Isbn as ISBN
-from .note import SectionNote as NotePart, Note, TextNote as Content
-from .orderer import Orderer, ScriptsOrderer
-from .scripts import Script, No, NoScripts
-from .scriptsprocess import Section, ScriptsProcess, By
-from .status import LabelStatus as Label, SectionStatus as ScriptsStatusCode, Status
+from .base import (
+    BaseCompactSerializer,
+    PolicyBasedSerializer,
+    BaseReadSerializer
+)
+from .common import (
+    SizeReadSerializer,
+    SizeWriteSerializer,
+    InstituteReadSerializer,
+    InstituteWriteSerializer
+)
+from .cover import (
+    CoverReadSerializer,
+    CoverWriteSerializer
+)
+from .description import (
+    TextReadSerializer, 
+    TextWriteSerializer,
+    DescriptionPartReadSerializer,
+    DescriptionPartWriteSerializer,
+    DescriptionReadSerializer,
+    DescriptionWriteSerializer
+)
+from .flag import (
+    PartReadSerializer,
+    PartWriteSerializer,
+    FlagReadSerializer,
+    FlagWriteSerializer
+)
+from .isbn import (
+    TypeReadSerializer,
+    TypeWriteSerializer,
+    ISBNReadSerializer,
+    ISBNWriteSerializer,
+
+)
+from .note import (
+    NotePartReadSerializer,
+    NotePartWriteSerializer,
+    ContentReadSerializer,
+    ContentWriteSerializer,
+    NoteReadSerializer,
+    NoteWriteSerializer
+)
+from .orderer import (
+    OrdererReadSerializer,
+    OrdererWriteSerializer,
+    ScriptOrdererReadSerializer,
+    ScriptOrdererWriteSerializer
+)
+from .scripts import (
+    ScriptsReadSerializer,
+    ScriptsWriteSerializer
+)
+from .scriptsprocess import (
+    SectionReadSerializer,
+    SectionWriteSerializer,
+    ByReadSerializer,
+    ByWriteSerializer,
+    ScriptsProcessReadSerializer,
+    ScriptsProcessWriteSerializer
+)
+from .status import (
+    LabelReadSerializer,
+    LabelWriteSerializer,
+    CodeReadSerializer,
+    CodeWriteSerializer,
+    StatusReadSerializer,
+    StatusWriteSerializer
+)
