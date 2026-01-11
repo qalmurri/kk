8ffe72763ae13c830b810c1dbe852509c4681b46
qalmurri <blog.qalmurri@gmail.com>
2026-01-11 19:48:17 +0700

    Testing Push

--- backend/kk/urls.py
+++ backend/kk/urls.py
@@ -1,10 +1,4 @@
 from django.urls import path, include
-# testing push/commit
-# testing ke dua
-# testing ke tiga
-# testing ke empat
-# testing ke lima
-# testing ke 6
 
 urlpatterns = [
     path('', include('scripts.urls'))
