dbf1d717b9caef08575840134b46168969c352cc
qalmurri <blog.qalmurri@gmail.com>
2026-02-19 11:44:38 +0700

    cl

--- app/app.py
+++ app/app.py
@@ -1,21 +1,3 @@
-# Untuk test github cli
-#
-# from PySide6.QtWidgets import QApplication
-# from core.session import Session
-# from views.auth.login_window import LoginWindow
-# from views.main.main_window import MainWindow
-# import sys
-# 
-# app = QApplication(sys.argv)
-# 
-# if Session.is_logged_in():
-#     window = MainWindow()
-# else:
-#     window = LoginWindow()
-# 
-# window.show()
-# sys.exit(app.exec())
-
 import sys
 import os
 
@@ -37,7 +19,6 @@ def main():
 
 
 if __name__ == "__main__":
-    # Kalau di CI, jangan jalankan GUI
     if os.environ.get("CI") == "true":
         print("Running in CI mode, GUI skipped")
     else:
